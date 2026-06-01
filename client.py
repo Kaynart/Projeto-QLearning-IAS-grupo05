75886751ee9e7693a49a14ca3cc189c5c4a9daaa
Kaynart <kaynancabj4b@gmail.com>
2026-06-01 14:58:23 -0300

    add: adiciona bloco de codigo inicial para geracao de acoes aleatorias

--- client.py
+++ client.py
@@ -1,1 +1,20 @@
+# Importações
+import connection as cn # importação do arquivo de connection para conexão local do jogo
+from random import randint # importação de gerador aleatório de números pra randomização da ação
+
+# Realização da conexão com o jogo através da porta
+conexao_jogo = cn.connect(2037)
+
+# Listagem de ações pra fazer a equivalência do random int (que será o índice) com a string de ação
+acoes = ["jump", "left", "right"]
+
+# Loop do jogo
+while True:
+    indice = randint(0,2) # pega o índice aleatório pra ação
+    acao_escolhida = acoes[indice] # obtendo, enfim, a ação randomizada
+
+    estado, recompensa = cn.get_state_reward(conexao_jogo, acao_escolhida) # função do conecction.py que devolve o novo estado e a recompensa recebida pela ação executada
+
+    print (f"O Amongois realizou a ação {acao_escolhida} e parou no estado {estado} com recompensa {recompensa}!") # impressão geral pra acompanhamento
+
 #Aqui vocês irão colocar seu algoritmo de aprendizado
